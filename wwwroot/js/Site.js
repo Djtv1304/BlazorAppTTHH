fb7f4bfa18d48da7a38b8606234d3ba0e7e2d7c9
Diego <diegosebastian_t@hotmail.com>
2023-11-30 09:27:21 -0500

    Add project files.

---
+++ wwwroot/js/Site.js
@@ -0,0 +1,11 @@
+﻿
+function viewProduct(productName, productDescription) {
+    $("#viewProductName").text(productName);
+    $("#viewProductDescription").text(productDescription);
+    $('#viewProductModal').modal('show');
+}
+
+function hideProductModal() {
+    $('#viewProductModal').modal('hide');
+}
+
